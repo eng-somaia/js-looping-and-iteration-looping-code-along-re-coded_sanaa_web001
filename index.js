aa5d5e97cd5e924dab310664b01593ce8b2fe08e
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-12 05:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(a,b){
+  let c = [];
+  for (let i=0; i<a.length;i++){
+    c[i] = console.log(`Thank you, ${a[i]}, for the wonderful ${b} gift!`);
+  }
+
+  return c;
+}
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+function countDown(n){
+  while (n>=0){
+    console.log(n);
+    n--;
+  }
+}
+countDown(19);
